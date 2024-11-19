eb8d6e96a5f0f05ce660d4037338eee5848f9bb9
Mansi Khamkar <mansi.khamkar@clarifai.com>
2024-11-19 21:19:19 +0530

    tested summarizer elements

---
+++ clarifai_datautils/multimodal/pipeline/summarizer.py
@@ -0,0 +1,87 @@
+from typing import List
+import os
+import random
+import base64
+
+try:
+  from unstructured.documents.elements import ElementMetadata, Image, CompositeElement
+except ImportError:
+  raise ImportError(
+      "Could not import unstructured package. "
+      "Please install it with `pip install 'unstructured[pdf] @ git+https://github.com/clarifai/unstructured.git@support_clarifai_model'`."
+  )
+
+from clarifai_datautils.constants.pipeline import MAX_NODES, SKIP_NODES
+from clarifai.client.model import Model
+from clarifai.client.input import Inputs
+
+from .basetransform import BaseTransform
+
+class ImageSummarizer(BaseTransform):
+  """ Summarizes image elements. """
+
+  def __init__(self, model_url="https://clarifai.com/qwen/qwen-VL/models/qwen-VL-Chat"):
+    """Initializes an LlamaIndexWrapper object.
+
+    Args:
+        model_url (str): Model URL to use for summarization.
+
+    """
+    self.model_url = model_url
+    self.model = Model(url=model_url, pat=os.environ.get("CLARIFAI_PAT"))
+    self.summary_prompt = """You are an assistant tasked with summarizing images for retrieval. \
+        These summaries will be embedded and used to retrieve the raw image. \
+        Give a concise summary of the image that is well optimized for retrieval."""
+
+  def __call__(self, elements: List) -> List:
+    """Applies the transformation.
+
+    Args:
+        elements (List[str]): List of text elements.
+
+    Returns:
+        List of transformed text elements.
+
+    """
+    img_elements = []
+    for _, element in enumerate(elements):
+      element.metadata.update(ElementMetadata.from_dict({'is_original': True}))
+      if isinstance(element, Image):
+        img_elements.append(element)
+    # new_elements = Parallel(n_jobs=len(elements))(delayed(self._summarize_image)(element) for element in img_elements)
+    new_elements = self._summarize_image(elements)
+    elements.extend(new_elements)
+    return elements
+  
+  def _summarize_image(self, image_elements: List[Image]) -> List[CompositeElement]:
+    """Summarizes an image element.
+
+    Args:
+        image_element (Image): Image element to summarize.
+
+    Returns:
+        Summarized image element.
+
+    """
+    img_inputs = []
+    for element in image_elements:
+        if not isinstance(element, Image):
+            continue
+        input_proto = Inputs.get_multimodal_input(input_id=f'{random.randint(1000000, 99999999)}', 
+                                               image_bytes=base64.b64decode(element.metadata.image_base64), 
+                                               raw_text=self.summary_prompt)
+        img_inputs.append(input_proto)
+    resp = self.model.predict(img_inputs)
+
+    new_elements = []
+    for i, element in enumerate(resp.outputs):
+        summary = ""
+        if image_elements[i].text:
+            summary = image_elements[i].text
+        summary = summary + " \n " +element.data.text.raw
+        eid =  image_elements[i].to_dict()['element_id']
+        meta_dict = {'source_element_id': eid, 'is_original': False}
+        comp_element = CompositeElement(text=summary, metadata=ElementMetadata.from_dict(meta_dict), element_id="summarized_" + eid)
+        new_elements.append(comp_element)
+    
+    return new_elements

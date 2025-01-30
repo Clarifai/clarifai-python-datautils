af7fbabf94b85c0fb4f9cbfb4bfe7c1af71fdb2e
Srikanth Bachala <srikanth.bachala@clarifai.com>
2025-01-30 16:52:32 +0530

    [WFP-900]: Fix issues in Multimodal Pipeline Tests

--- tests/pipelines/test_multimodal_pipelines.py
+++ tests/pipelines/test_multimodal_pipelines.py
@@ -5,7 +5,6 @@ PDF_FILE_PATH = osp.abspath(
     osp.join(osp.dirname(__file__), "assets", "Multimodal_sample_file.pdf"))
 
 
-@pytest.mark.skip(reason="Need additional build dependencies")
 class TestMultimodalPipelines:
   """Tests for pipeline transformations."""
 
@@ -66,6 +65,7 @@ class TestMultimodalPipelines:
     assert len(elements) == 14
     assert elements.elements[0].metadata.to_dict()['filename'] == 'Multimodal_sample_file.pdf'
 
+  @pytest.mark.skip(reason="Need additional setup")
   def test_pipeline_summarize(self,):
     """Tests for pipeline run with summarizer"""
     import os
